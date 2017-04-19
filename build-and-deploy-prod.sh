a983ebc5602d348aac63f797101adeb627d8fc1c
Toby Sullivan <tobyjsullivan@gmail.com>
2017-04-18 21:12:45 -0700

    Update api url

--- build-and-deploy-prod.sh
+++ build-and-deploy-prod.sh
@@ -1,2 +1,2 @@
-REACT_APP_API_BASE_URL=https://ocs-api.herokuapp.com npm run build && aws s3 cp ./build s3://oneclicksandwich.com/ --recursive
+REACT_APP_API_BASE_URL=https://api.oneclicksandwich.com npm run build && aws s3 cp ./build s3://oneclicksandwich.com/ --recursive
 
